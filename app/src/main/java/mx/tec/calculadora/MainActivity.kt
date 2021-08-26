cd4535597c8d132d87ea3e592dc2ff757b7660b3
Jonathan Maya <yony_ms@live.com>
2021-08-26 15:45:04 -0500

    Android Calculator Final

--- app/src/main/java/mx/tec/calculadora/MainActivity.kt
+++ app/src/main/java/mx/tec/calculadora/MainActivity.kt
@@ -1,5 +1,6 @@
 package mx.tec.calculadora
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,8 +10,46 @@ import com.notkamui.keval.Keval
 import com.notkamui.keval.KevalZeroDivisionException
 import kotlinx.android.synthetic.main.activity_main.*
 import org.w3c.dom.Text
+import kotlin.concurrent.fixedRateTimer
+import kotlin.math.round
+
+fun isDecimal(x:String):Boolean{
+    val decimalPoint = '.'
+    val inString = x.indexOf(decimalPoint)
+    if(inString == -1){
+        return false
+    } else{
+        return true
+    }
+}
+
+fun firstDigit(x: String): Boolean{
+    val operatorArray = arrayOf('/', '*', '+', '-')
+    val firstPos = x.first()
+    val inArray = operatorArray.indexOf(firstPos)
+    if(inArray == -1){
+        return false
+    } else{
+        return true
+    }
+}
+
+fun lastDigit(x: String): String{
+    val operatorArray = arrayOf('/', '*', '+', '-', '.')
+    val lastPos = x.last()
+    val inArray = operatorArray.indexOf(lastPos)
+
+    if(inArray == -1){
+        return x
+    } else{
+        return x.dropLast(1)
+    }
+}
 
 var onScreenRes = false
+var isMathOperator = false
+var lastMathOp = ""
+
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     lateinit var strEvaluate: String
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,142 +93,213 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         strEvaluate = ""
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onClick(p0: View?) {
         when(p0!!.id){
             R.id.buttonZero -> {
                 if(onScreenRes){
-                    textViewResult.setText("")
+                    strEvaluate = ""
+                    textViewResult.text = ""
                     onScreenRes = false
+                    lastMathOp = ""
                 }
                 textViewResult.text = textViewResult.text.toString() + buttonZero.text
                 strEvaluate += buttonZero.text
+                lastMathOp += buttonZero.text
+                isMathOperator = false
             }
             R.id.buttonOne -> {
                 if(onScreenRes){
-                    textViewResult.setText("")
+                    strEvaluate = ""
+                    textViewResult.text = ""
                     onScreenRes = false
+                    lastMathOp = ""
                 }
                 textViewResult.text = textViewResult.text.toString() + buttonOne.text
                 strEvaluate += buttonOne.text
+                lastMathOp += buttonOne.text
+                isMathOperator = false
             }
             R.id.buttonTwo -> {
                 if(onScreenRes){
-                    textViewResult.setText("")
+                    strEvaluate = ""
+                    textViewResult.text = ""
                     onScreenRes = false
+                    lastMathOp = ""
                 }
                 textViewResult.text = textViewResult.text.toString() + buttonTwo.text
                 strEvaluate += buttonTwo.text
-
+                lastMathOp += buttonTwo.text
+                isMathOperator = false
             }
             R.id.buttonThree -> {
                 if(onScreenRes){
-                    textViewResult.setText("")
+                    strEvaluate = ""
+                    textViewResult.text = ""
                     onScreenRes = false
+                    lastMathOp = ""
                 }
                 textViewResult.text = textViewResult.text.toString() + buttonThree.text
                 strEvaluate += buttonThree.text
-
+                lastMathOp += buttonThree.text
+                isMathOperator = false
             }
             R.id.buttonFour -> {
                 if(onScreenRes){
-                    textViewResult.setText("")
+                    strEvaluate = ""
+                    textViewResult.text = ""
                     onScreenRes = false
+                    lastMathOp = ""
                 }
                 textViewResult.text = textViewResult.text.toString() + buttonFour.text
                 strEvaluate += buttonFour.text
-
+                lastMathOp += buttonFour.text
+                isMathOperator = false
             }
             R.id.buttonFive -> {
                 if(onScreenRes){
-                    textViewResult.setText("")
+                    strEvaluate = ""
+                    textViewResult.text = ""
                     onScreenRes = false
+                    lastMathOp = ""
                 }
                 textViewResult.text = textViewResult.text.toString() + buttonFive.text
                 strEvaluate += buttonFive.text
-
+                lastMathOp += buttonFive.text
+                isMathOperator = false
             }
             R.id.buttonSix -> {
                 if(onScreenRes){
-                    textViewResult.setText("")
+                    strEvaluate = ""
+                    textViewResult.text = ""
                     onScreenRes = false
+                    lastMathOp = ""
                 }
                 textViewResult.text = textViewResult.text.toString() + buttonSix.text
                 strEvaluate += buttonSix.text
-
+                lastMathOp += buttonSix.text
+                isMathOperator = false
             }
             R.id.buttonSeven -> {
                 if(onScreenRes){
-                    textViewResult.setText("")
+                    strEvaluate = ""
+                    textViewResult.text = ""
                     onScreenRes = false
+                    lastMathOp = ""
                 }
                 textViewResult.text = textViewResult.text.toString() + buttonSeven.text
                 strEvaluate += buttonSeven.text
+                lastMathOp += buttonSeven.text
+                isMathOperator = false
             }
             R.id.buttonEight -> {
                 if(onScreenRes){
-                    textViewResult.setText("")
+                    strEvaluate = ""
+                    textViewResult.text = ""
                     onScreenRes = false
+                    lastMathOp = ""
                 }
                 textViewResult.text = textViewResult.text.toString() + buttonEight.text
                 strEvaluate += buttonEight.text
+                lastMathOp += buttonEight.text
+                isMathOperator = false
             }
             R.id.buttonNine -> {
                 if(onScreenRes){
-                    textViewResult.setText("")
+                    strEvaluate = ""
+                    textViewResult.text = ""
                     onScreenRes = false
+                    lastMathOp = ""
                 }
                 textViewResult.text = textViewResult.text.toString() + buttonNine.text
                 strEvaluate += buttonNine.text
+                lastMathOp += buttonNine.text
+                isMathOperator = false
             }
             R.id.buttonDecimal -> {
                 if(onScreenRes){
-                    textViewResult.setText("")
+                    textViewResult.text = ""
                     onScreenRes = false
+                    strEvaluate = ""
+                    println("2")
                 }
-                textViewResult.text = textViewResult.text.toString() + buttonDecimal.text
-                strEvaluate += buttonDecimal.text
+                if(isDecimal(lastMathOp)){
+                    println("No pasa nada")
+                } else{
+                    textViewResult.text = textViewResult.text.toString() + buttonDecimal.text
+                    strEvaluate += buttonDecimal.text
+                    lastMathOp += buttonDecimal.text
+                }
             }
             R.id.buttonEqual -> {
-                try {
-                    val operationRes = Keval.eval(strEvaluate)
-                    textViewResult.setText(operationRes.toString())
-                    strEvaluate = ""
-                    onScreenRes = true
-                } catch (exception: KevalZeroDivisionException){
-                    textViewResult.setText("Error")
+                if (strEvaluate.isEmpty()){
+                    println("No pasa nada.")
+                } else {
+                    strEvaluate = lastDigit(strEvaluate)
+                    println(strEvaluate)
+                    println(lastMathOp)
+                    try {
+                        if(onScreenRes && firstDigit(lastMathOp)){
+                            strEvaluate += lastMathOp
+                        }
+                        var operationRes = Keval.eval(strEvaluate)
+                        operationRes = String.format("%.4f", operationRes).toDouble()
+                        textViewResult.text = operationRes.toString()
+                        strEvaluate = operationRes.toString()
+                        onScreenRes = true
+                    } catch (exception: KevalZeroDivisionException) {
+                        textViewResult.text = "Error"
+                        onScreenRes = true
+                    }
                 }
             }
             R.id.buttonPlus -> {
                 if(onScreenRes){
-                    textViewResult.setText("")
                     onScreenRes = false
                 }
-                textViewResult.text = textViewResult.text.toString() + buttonPlus.text
-                strEvaluate += buttonPlus.text
+                if(!isMathOperator) {
+                    textViewResult.text = textViewResult.text.toString() + buttonPlus.text
+                    strEvaluate += buttonPlus.text
+                    lastMathOp = ""
+                    lastMathOp += buttonPlus.text
+                    isMathOperator = true
+                }
             }
             R.id.buttonMinus -> {
                 if(onScreenRes){
-                    textViewResult.setText("")
                     onScreenRes = false
                 }
-                textViewResult.text = textViewResult.text.toString() + buttonMinus.text
-                strEvaluate += buttonMinus.text
+                if(!isMathOperator) {
+                    textViewResult.text = textViewResult.text.toString() + buttonMinus.text
+                    strEvaluate += buttonMinus.text
+                    lastMathOp = ""
+                    lastMathOp += buttonMinus.text
+                    isMathOperator = true
+                }
             }
             R.id.buttonDiv -> {
                 if(onScreenRes){
-                    textViewResult.setText("")
                     onScreenRes = false
                 }
-                textViewResult.text = textViewResult.text.toString() + buttonDiv.text
-                strEvaluate += buttonDiv.text
+                if(!isMathOperator) {
+                    textViewResult.text = textViewResult.text.toString() + buttonDiv.text
+                    strEvaluate += buttonDiv.text
+                    lastMathOp = ""
+                    lastMathOp += buttonDiv.text
+                    isMathOperator = true
+                }
             }
             R.id.buttonMult -> {
                 if(onScreenRes){
-                    textViewResult.setText("")
                     onScreenRes = false
                 }
-                textViewResult.text = textViewResult.text.toString() + buttonMult.text
-                strEvaluate += buttonMult.text
+                if(!isMathOperator) {
+                    textViewResult.text = textViewResult.text.toString() + buttonMult.text
+                    strEvaluate += buttonMult.text
+                    lastMathOp = ""
+                    lastMathOp += buttonMult.text
+                    isMathOperator = true
+                }
             }
         }
     }
